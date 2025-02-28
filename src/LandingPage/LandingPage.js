b07fee3da8e9af58e212a1847a77ee4312887c05
Anup Padakandla <anuppadakandla064@gmail.com>
2025-02-28 15:28:21 +0530

    landing page with section1and2 added

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import "./LandingPage.scss";
+import React from "react";
+import Section1 from "../Section1/Section1";
+import Section2 from "../Section2/Section2";
+
+function LandingPage() {
+    return (
+        <div>
+            <Section1/>
+            <Section2/>
+        </div>
+    )
+}
+
+export default LandingPage;
---
+++ src/Section2/Section2.js
@@ -0,0 +1,12 @@
+import './Section2.scss';       
+import React from 'react';
+
+function Section2() {
+    return (
+        <div className="section2-div">
+            <h1>Work Experience...</h1>
+        </div>
+    )
+}
+
+export default Section2;
